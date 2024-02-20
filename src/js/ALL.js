11f4faaaa782216cacc0a7c88fe8e1cdeae5ae95
Kayky Guilherme <130869882+KaykyGuilherme@users.noreply.github.com>
2024-02-19 21:32:54 -0300

    1

--- src/js/ALL.js
+++ src/js/ALL.js
@@ -1,3 +1,6 @@
+// =======================================
+// Menu hamburger mobile
+
 const headerALL = {
     navALL: document.querySelector('nav'),
     btnMobile: document.querySelector('.menu__mobile'),
@@ -6,3 +9,24 @@ const headerALL = {
 headerALL.btnMobile.addEventListener('click', () =>{
     headerALL.navALL.classList.toggle('nav__mobile__active')
 })
+
+// =======================================
+// Maquina de escrever
+
+const escritasSalva = ['Front-End', 'dev-game', 'mobile', 'full-stack']
+let numeroEscrita = 0
+const escrever = document.querySelector('.esrever')
+
+const maquinaDeEscrever = () =>{
+    
+    escrever.textContent = ''
+    const separarEscrita = escritasSalva[numeroEscrita].split('')
+    
+    separarEscrita.forEach((letra, numero) =>{
+      
+    })
+
+    trocarEscrita()
+}
+
+maquinaDeEscrever()
